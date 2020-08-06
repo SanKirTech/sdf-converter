32e589211e2dd7b937c79b89f950fd70976e2df3
Sanjay <sanjay@sankir.com>
2020-08-06 14:42:01 +0530

    Commented out apt part

--- startupScript.sh
+++ startupScript.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-apt update
-apt -y install python3 python3-pip git
+
+# apt update
+# apt -y install python3 python3-pip git
 git clone https://github.com/SanKirTech/sdf-converter.git
 python3 -m pip install -r sdf-converter/requirements.txt
